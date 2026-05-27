8bf4618f394f0aa1623ee9f8d794e647e4ab5ca8
gh0stp4wz <gh0st13@gh0stp4wz.is-a.dev>
2026-05-27 10:34:44 +0200

    release flipcord

---
+++ README.md
@@ -0,0 +1,3 @@
+# flipcord
+
+flips the discord layout from left to right
---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "flipcord",
+		author: "Nexus-Themes",
+		version: "1.0.0",
+		description: "flips the discord layout from left to right",
+		source: "https://github.com/Nexus-Themes/flipcord"
+	}
+}
